RisingOS-staging/android_vendor_lineage

config: Remove useless addon.d file
* Avoid detection as custom ROM

Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

config/common.mk
@@ -41,11 +41,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/lineage/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/lineage/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/lineage/prebuilt/common/bin/50-lineage.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-lineage.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-lineage.sh
+    vendor/lineage/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \

venudinesh/vendor_infinity

config/common.mk
@@ -38,11 +38,7 @@ include vendor/extras/prebuilts.mk
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/infinity/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/infinity/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/infinity/prebuilt/common/bin/50-infinity.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-infinity.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-infinity.sh
+    vendor/infinity/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \